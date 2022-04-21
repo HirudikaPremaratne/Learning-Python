04e28cee4e7d16533be24993ee8abeaec1f033de
HirudikaPremaratne <83544267+HirudikaPremaratne@users.noreply.github.com>
2022-04-21 11:06:04 +0530

    input

---
+++ inputs.py
@@ -0,0 +1,16 @@
+#Input name
+print("Input Name")
+name = input()
+print(name)
+
+#Input and calculate sum of 2 numbers
+print("input first number")
+no1 = int(input())
+print("input second number")
+no2 = input()
+
+sum = no1 + int(no2)
+
+print("Sum is ", int(sum))
+
+
